9b54c2ebd45eccf81c4f5a60d181cc0a0f579028
M1n3rv4-Ln <13758021513@163.com>
2026-04-08 14:29:41 +0800

    重构demo

--- agents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Agents模块"""
-
-from .scanner_agent import ScannerAgent
-from .planner_agent import PlannerAgent
-from .executor_agent import ExecutorAgent
-
-__all__ = ["ScannerAgent", "PlannerAgent", "ExecutorAgent"]
--- skills/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Nmap Skill - 端口扫描工具集"""
-
-from .nmap_tool import NmapTool, QuickNmapTool, FullNmapTool
-
-__all__ = ["NmapTool", "QuickNmapTool", "FullNmapTool"]
